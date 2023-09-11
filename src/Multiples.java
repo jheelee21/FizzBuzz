bd7cd0141e2c4cc5360b9f4535808bc848f3b34e
Junhee <junheelee.lee@mail.utoronto.ca>
2023-09-11 12:33:01 -0400

    added new variable n

--- src/Multiples.java
+++ src/Multiples.java
@@ -3,8 +3,9 @@ public class Multiples {
     public static void main(String[] args) {
 
         int count = 0;
+        int n = 1000;
 
-        for (int i = 3; i < 1000; i++) {
+        for (int i = 3; i < n; i++) {
 
             boolean multiple3 = i % 3 == 0;
             boolean multiple5 = i % 5 == 0;
